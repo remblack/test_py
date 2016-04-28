0368aebc81a11d8ef5d583961eaa50c046f7c022
remblack <nicolas.gonzalez.arancibia@gmail.com>
2016-04-28 15:28:27 -0600

    Delete edrms_tasklist.sh

--- edrms_tasklist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script retrieve the "my Task" list of an specific user and place it in a folder within alfresco
-curl -o /mnt/alfresco/student-records/myTask/task_list_ro_`date +"%A"`.json -u rofresco:fr3sh@1r http://localhost:8080/alfresco/service/api/task-instances
