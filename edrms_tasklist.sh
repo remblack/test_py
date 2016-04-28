d07cb3c69622b9cff5349afdc683dca7b606251f
unknown <Nick@.(none)>
2016-04-28 08:56:51 -0600

    -- ngonzale -- Initial commit

---
+++ edrms_tasklist.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script retrieve the "my Task" list of an specific user and place it in a folder within alfresco
+curl -o /mnt/alfresco/student-records/myTask/task_list_ro_`date +"%A"`.json -u rofresco:fr3sh@1r http://localhost:8080/alfresco/service/api/task-instances
