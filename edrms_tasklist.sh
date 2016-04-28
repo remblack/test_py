6d39d9fdcca96dcf26d4502efedbc7d2ba9ced67
remblack <nicolas.gonzalez.arancibia@gmail.com>
2016-04-28 16:39:29 -0600

    Update edrms_tasklist.sh

--- edrms_tasklist.sh
+++ edrms_tasklist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# new comments
 #This is a new comment that need to be commited
 
 # This script retrieve the "my Task" list of an specific user and place it in a folder within alfresco
